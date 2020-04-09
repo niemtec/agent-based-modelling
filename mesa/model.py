8a8832e1aec845d8fd8cd10393c04c00e2b16d85
Jake <janiemiec20@gmail.com>
2020-04-09 19:57:06 +0100

    Updated changes to the files based on tutorial smaple

--- mesa/model.py
+++ mesa/model.py
@@ -1,72 +1,91 @@
-from mesa.space import SingleGrid
+from mesa import Model, Agent
 from mesa.time import RandomActivation
+from mesa.space import SingleGrid
 from mesa.datacollection import DataCollector
+
+
 class SchellingAgent(Agent):
-   # Initialisation
+   '''
+   Schelling segregation agent
+   '''
+
    def __init__(self, pos, model, agent_type):
+      '''
+      Create a new Schelling agent.
+      Args:
+         unique_id: Unique identifier for the agent.
+         x, y: Agent initial location.
+         agent_type: Indicator for the agent's type (minority=1, majority=0)
+      '''
       super().__init__(pos, model)
       self.pos = pos
       self.type = agent_type
 
-   # Step function
    def step(self):
       similar = 0
-      # Calculate number of similar neighbours
-      for neighbour in self.model.grid.neighbour_iter(self.pos):
-         if neighbour.type == self.type:
-            similar += 1
+      for neighbor in self.model.grid.neighbor_iter(self.pos):
+         if neighbor.type == self.type:
+               similar += 1
 
-      # Move to a random empty location if unhappy
-      if similar < self.model.homphily:
+      # If unhappy, move:
+      if similar < self.model.homophily:
          self.model.grid.move_to_empty(self)
       else:
          self.model.happy += 1
 
+
 class Schelling(Model):
-   def __init__(height, width, density, minority_pc, homophily):
-      # Width: Horizontal axis
-      # Height: Vertical axis
-      # Density: Population density (0-1)
-      # Fraction Minority: Ratio between red/blue (blue being minority)
-      # Homophily: Number of similar neighbours required for agents to be happy (0-8)
+   '''
+   Model class for the Schelling segregation model.
+   '''
+
+   def __init__(self, height=20, width=20, density=0.8, minority_pc=0.2, homophily=3):
+      '''
+      '''
+
       self.height = height
       self.width = width
       self.density = density
       self.minority_pc = minority_pc
       self.homophily = homophily
 
-      # Grid setup
-      self.grid = SingleGrid(width, height, torus=True)
-      
-      # Scheduler setup
       self.schedule = RandomActivation(self)
+      self.grid = SingleGrid(width, height, torus=True)
 
-      # Data collection setup
       self.happy = 0
-      self.datacollector = DataCollector({"happy": "happy"}, {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1]})
-      
-      # Agents setup
+      self.datacollector = DataCollector(
+         {"happy": "happy"},  # Model-level count of happy agents
+         # For testing purposes, agent's individual x and y
+         {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1]})
+
+      # Set up agents
+      # We use a grid iterator that returns
+      # the coordinates of a cell as well as
+      # its contents. (coord_iter)
       for cell in self.grid.coord_iter():
          x = cell[1]
          y = cell[2]
          if self.random.random() < self.density:
-            if self.random.random() < self.minority_pc:
-               agent_type = 1
-            else:
-               agent_type = 0
-         
-         agent = SchellingAgent((x, y), self, agent_type)
-         self.grid.position_agent(agent, (x, y))
-         self.schedule.add(agent)
+               if self.random.random() < self.minority_pc:
+                  agent_type = 1
+               else:
+                  agent_type = 0
 
-         self.running = True
-         self.datacollector.collect(self)
+               agent = SchellingAgent((x, y), self, agent_type)
+               self.grid.position_agent(agent, (x, y))
+               self.schedule.add(agent)
+
+      self.running = True
+      self.datacollector.collect(self)
 
    def step(self):
-      self.happy = 0  # Reset the counter of happy agents
+      '''
+      Run one step of the model. If All agents are happy, halt the model.
+      '''
+      self.happy = 0  # Reset counter of happy agents
       self.schedule.step()
-      # Collect data
+      # collect data
       self.datacollector.collect(self)
-      # Stop the model if all agents are happy
+
       if self.happy == self.schedule.get_agent_count():
          self.running = False
--- mesa/server.py
+++ mesa/server.py
@@ -1,31 +1,42 @@
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid, ChartModule, TextElement
 from mesa.visualization.UserParam import UserSettableParameter
+
 from model import Schelling
 
+
 class HappyElement(TextElement):
+   '''
+   Display a text count of how many happy agents there are.
+   '''
+
    def __init__(self):
       pass
 
    def render(self, model):
       return "Happy agents: " + str(model.happy)
 
-   def schelling_draw(agent):
-      if agent is None:
-         return
-      portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 0}
 
-      if agent.type == 0:
-         portrayal["Color"] = ["#FF0000", "#FF9999"]
-         portrayal["stroke_color"] = "#00FF00"
-      else:
-         portrayal["Color"] = ["#0000FF", "#9999FF"]
-         portrayal["stroke_color"] = "#000000"
-      return portrayal
+def schelling_draw(agent):
+   '''
+   Portrayal Method for canvas
+   '''
+   if agent is None:
+      return
+   portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 0}
+
+   if agent.type == 0:
+      portrayal["Color"] = ["#FF0000", "#FF9999"]
+      portrayal["stroke_color"] = "#00FF00"
+   else:
+      portrayal["Color"] = ["#0000FF", "#9999FF"]
+      portrayal["stroke_color"] = "#000000"
+   return portrayal
+
 
 happy_element = HappyElement()
 canvas_element = CanvasGrid(schelling_draw, 20, 20, 500, 500)
-happy_chart = ChartModule([{"Label": "happy", "Colour":"Black"}])
+happy_chart = ChartModule([{"Label": "happy", "Color": "Black"}])
 
 model_params = {
    "height": 20,
@@ -35,4 +46,6 @@ model_params = {
    "homophily": UserSettableParameter("slider", "Homophily", 3, 0, 8, 1)
 }
 
-server = ModularServer(Schelling, [canvas_element, happy_element, happy_chart], "Schelling", model_params)
+server = ModularServer(Schelling,
+                     [canvas_element, happy_element, happy_chart],
+                     "Schelling", model_params)
